67ecf84c8141028da98e8d23314c56456e280de0
John Frankel <john.frankel@omadahealth.com>
2014-10-17 08:58:02 -0700

    Adding basic xcode project

---
+++ WeightBuddy/WeightBuddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeightBuddy
+//
+//  Created by John Frankel on 10/17/14.
+//  Copyright (c) 2014 John Frankel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeightBuddy/WeightBuddy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeightBuddy
+//
+//  Created by John Frankel on 10/17/14.
+//  Copyright (c) 2014 John Frankel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
