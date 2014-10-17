5e17506a2e376323e57f140e8705ec2b2e42d78c
John Frankel <john.frankel@omadahealth.com>
2014-10-17 10:35:23 -0700

    Adding basic xcode project

---
+++ HealthyFoodFinder/HealthyFoodFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthyFoodFinder
+//
+//  Created by John Frankel on 10/17/14.
+//  Copyright (c) 2014 John Frankel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthyFoodFinder/HealthyFoodFinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HealthyFoodFinder
+//
+//  Created by John Frankel on 10/17/14.
+//  Copyright (c) 2014 John Frankel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
